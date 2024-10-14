6c0d02b9afff0d91eb786eaaa780d00c386320e3
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-10-14 11:05:31 +0200

    Add Category Entity

---
+++ src/main/java/com/java/platform/model/Category.java
@@ -0,0 +1,58 @@
+package com.java.platform.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "categories")
+public class Category 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotNull
+	@NotEmpty
+	@Size(min = 3, max = 20)
+	//@Column(name = "name", nullable = false)
+	private String name;
+	
+	//@ManyToMany(mappedBy = "categories")
+	//@JoinColumn(name = "tickets_id", nullable = false)
+	//@JsonBackReference
+	//private List<Ticket> tickets;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+//	public List<Ticket> getTickets() {
+//		return tickets;
+//	}
+//
+//	public void setTickets(List<Ticket> tickets) {
+//		this.tickets = tickets;
+//	}
+	
+}
